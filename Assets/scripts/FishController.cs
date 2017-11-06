c26a4892b6efa65dbb0de615b5f1cfd38e491a29
Kirill Meng <kimeng@htwg-konstanz.de>
2017-11-06 15:38:59 +0100

    - add fish controller - add wall trigger - add external resources (sardine, steamvr) - restructuring folders - add asset folders

---
+++ Assets/scripts/FishController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishController : MonoBehaviour {
+    public float velocity = 0.01F;
+    public float scale = 1F;
+    private void Start()
+    {
+        transform.localScale = new Vector3(scale/4, scale/5, scale);
+        transform.position = new Vector3(Random.Range(1, 9), Random.Range(1, 9), Random.Range(1, 9));
+        transform.Rotate(Random.Range(1, 180), Random.Range(1, 180), Random.Range(1, 180));
+    }
+    // Update is called once per frame
+    void Update () {
+        transform.Translate(0, 0, velocity*Time.deltaTime);
+    } 
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("verticalPlane"))
+        {
+            transform.Rotate(0, transform.rotation.y + Random.Range(70,100), 0);
+        }
+
+        if (other.gameObject.CompareTag("horizontalPlane"))
+        {
+            transform.Rotate(transform.rotation.x + Random.Range(70, 100), 0, transform.rotation.z + Random.Range(70, 100));
+        }
+    }  
+}
---
+++ Assets/scripts/wallTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class wallTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var chaperone = OpenVR.Chaperone;
+        HmdQuad_t pRect = new HmdQuad_t();
+        bool success = (chaperone != null) && chaperone.GetPlayAreaRect(ref pRect);
+        if (!success)
+            Debug.LogWarning("Failed to get Calibrated Play Area bounds!  Make sure you have tracking first, and that your space is calibrated.");
+
+        //todo dynamic sizing
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
